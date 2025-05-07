500639085b34e2739f04e8146862e063d9afcc2a
anhmt <maituananh12061998@gmail.com>
2025-05-07 21:29:45 +0700

    update ui

--- src/pages/ItemDetail.tsx
+++ src/pages/ItemDetail.tsx
@@ -9,12 +9,23 @@ function ItemDetail() {
                 <div className="col-span-5 bg-white w-full rounded-lg">
                     <div className="flex justify-center">
                         <img 
-                        className="w-56 h-340"
+                        className="w-full h-full"
                         src="https://cdn1.fahasa.com/media/catalog/product/t/_/t_-tr_-th_ng-gi_m-4.jpg"/>
+                    </div>
+                    <div className="flex justify-center mt-4">
+                        {
+                            [1, 2, 3, 4, 5].map((item, index) => (
+                                <div key={index} className="ml-2">
+                                    <img 
+                                    className="w-20 h-20 bg-no-repeat bg-center bg-contain"
+                                    src="https://cdn1.fahasa.com/media/catalog/product/t/_/t_-tr_-th_ng-gi_m-4.jpg"/>
+                                </div>
+                            ))
+                        }
                     </div>
                 </div>
 
-                <div className="col-span-7 bg-white rounded-lg">
+                <div className="col-span-7 bg-white rounded-lg h-max">
                     <div className="ml-4">
                         <div className="flex items-center">
                             <div className="flex items-center justify-center bg-orange-235 text-white w-24 h-5 rounded-md">
@@ -37,15 +48,7 @@ function ItemDetail() {
                             <p className="text-3xl font-medium text-red-700">60.000 đ</p>
                             <del className="text-gray-500 ml-3">60.000 đ</del>
                             <div className="bg-red-700 text-white rounded-md ml-3 w-10">
-                                <p>-50%</p>
-                            </div>
-                        </div>
-                        <div className="flex justify-between">
-                            <div className="">
-                                <p>74 nhà sách còn hàng</p>
-                            </div>
-                            <div>
-                                <p>Sản phẩm hết hàng</p>
+                                <p className="text-center">-50%</p>
                             </div>
                         </div>
                     </div>
